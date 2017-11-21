d310cf7e61c1e62479e09130a800fde94a5b691e
Jessica Lovell <jessicalovell@Jessicas-MBP-2.home>
2017-11-20 23:50:19 -0600

    four loop

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+var result;
+var lost;
+var add;
+
+for(var i = 0; i <4; i++) {
+	
+	var crystal = $("<div>");
+
+	$(".crystals").append(crystal);
+}
+//4 crystals
+// Each crystal needs to have a random number between 1 - 12
+// The total number needs to randomly generate a number between 19-120. 
+// A new number regenerates every single time for each crystal whe the game restarts
+// Clicking on singular crystal adds to your points. 
+// If the numbers add up to the total score then the player wins.
+// If they go over the total score the player loses.
+// Score tracker. 
+
+
