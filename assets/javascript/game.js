4db06b2bf3cd16ebf0e2b3db985cd76fd380859f
Jessica Lovell <jessicalovell@Jessicas-MacBook-Pro-2.local>
2017-12-01 14:25:03 -0600

    starting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,22 @@
 var result;
 var lost;
 var add;
-
+//4 crystals
 for(var i = 0; i <4; i++) {
 	
+	var randomNumber = Math.floor(Math.random() * 12);
+	console.log(random);
+
 	var crystal = $("<div>");
+
+		crystal.attr("class", 'crystal');
 
 	$(".crystals").append(crystal);
 }
-//4 crystals
+
 // Each crystal needs to have a random number between 1 - 12
+
+
 // The total number needs to randomly generate a number between 19-120. 
 // A new number regenerates every single time for each crystal whe the game restarts
 // Clicking on singular crystal adds to your points. 
